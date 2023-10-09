4061e15b7ad28bcc2701fef963813e5aeb58e71b
hwifuq <lengochuy6829@gmail.com>
2023-10-09 11:22:45 +0700

    MENU UPDATE#2

---
+++ BattleShip/BattleShip/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BattleShip
+{
+    public partial class Form1 : Form
+    {
+
+        List<Panel> listPanel = new List<Panel>();
+        int index=0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panelPlay.Hide();
+            panelSetting.Hide();
+            panel1.BringToFront();
+        }
+
+        private void play_Click(object sender, EventArgs e)
+        {
+            panelPlay.Visible = true;
+        }
+
+        private void setting_Click(object sender, EventArgs e)
+        {
+            panelSetting.Visible = true;
+        }
+
+        private void btnPreviousSetting_Click(object sender, EventArgs e)
+        {
+            panelPlay.Visible = false;
+            panelSetting.Visible = false;
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            panelPlay.Visible = false;
+            panelSetting.Visible = false;
+        }
+    }
+}
